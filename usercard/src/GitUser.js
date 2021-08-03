9b026d4e13f63a5203b24247558b20b3e7faca9e
Jesse <eoshout@Gmail.com>
2021-08-03 16:03:03 -0400

    git user is displaying my name with axios

--- usercard/src/GitUser.js
+++ usercard/src/GitUser.js
@@ -4,13 +4,30 @@ import axios from "axios";
 
 class GitUser extends React.Component{
 
+    state = {
+        info: [{}],
+    };
+
+    componentDidMount(){
+        axios
+        .get("https://api.github.com/users/jessejohns-1")
+        .then ((res) => {
+            this.setState({
+                info: res.data,
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
 
 
 
     render(){
         return(
             <div>
-                <h2>THIS IS WHERE MY DATA GONE BE </h2>
+                <h2>{this.state.info.login}</h2>
             </div>
         )
     }
