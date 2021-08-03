308a235a2d0d2cae45563f7d9f6e1322fd5d2577
Jesse <eoshout@Gmail.com>
2021-08-03 15:46:16 -0400

    declared my app and rendered app

---
+++ usercard/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+
+class App extends React.Component{
+  
+state = {};
+
+componentDidMount() {};
+
+render() {
+  return(
+    <div>
+      <h1>Githubby User Cards</h1>
+      <GitUser/>
+    </div>
+  )
+}
+
+
+}
+export default App;
---
+++ usercard/src/GitUser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+
+
+class GitUser extends React.Component{
+
+
+
+
+    render(){
+        return(
+            <div>
+                <h2>ace</h2>
+            </div>
+        )
+    }
+}
+
+
+
+export default GitUser
